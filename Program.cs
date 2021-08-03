ebfcffb2db7b069d9a252c9bbfa38943e3935d49
DESKTOP-CQHE9TG\yeeso <yeesonmin@naver.com>
2021-08-03 23:11:44 +0900

    2. Add Two Numbers 풀이

--- Program.cs
+++ Program.cs
@@ -6,21 +6,69 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ListNode l1 = new ListNode();
+            Solution solution = new Solution();
 
-            Console.WriteLine(l1.val);
+            ListNode l1 = new ListNode(9);
+            l1.next = new ListNode(9);
+            l1.next.next= new ListNode(9);
+           
+            ListNode l2 = new ListNode(9);
+            l2.next = new ListNode(9);
+            ListNode answer = solution.AddTwoNumbers(l1, l2);
 
-            l1.next = new ListNode(1);
-            Console.WriteLine(l1.val);
+            while (answer.next != null)
+            {
+                Console.WriteLine(answer.val);
+                
+            }
+            
+            
 
-            l1 = l1.next;
-            Console.WriteLine(l1.val);
 
+           
 
         }
 
+
+
         
     }
+
+    public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        int sum= 0;
+        ListNode answer = new ListNode();
+        ListNode result = answer;
+
+            while (l1 != null || l2 != null)
+            {
+                 
+                if (l1 != null)
+                {
+                    sum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    sum += l2.val;
+                    l2 = l2.next;
+                }
+
+                answer.next = new ListNode(sum % 10);
+                sum /= 10;
+
+                answer = answer.next;
+
+            }
+
+            if(sum > 0){
+                answer.next = new ListNode(sum % 10);
+            }
+
+        return result.next;
+    }
+}
 
 
     public class ListNode
